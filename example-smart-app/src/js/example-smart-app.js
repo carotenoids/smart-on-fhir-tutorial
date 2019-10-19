c3e390da38dda6e7971277a4363ab7bb5099f383
Ryan Park <ryanpark@Ryans-iMac.local>
2019-10-19 00:36:37 -0400

    fix weight display

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -17,7 +17,8 @@
                       code: {
                         $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4',
+                              'http://loinc.org|29463-7']
                       }
                     }
                   });
@@ -27,7 +28,6 @@
         $.when(pt, obv).done(function(patient, obv) {
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
-          var weight = patient.weight; //ryan
 
           var fname = '';
           var lname = '';
@@ -38,6 +38,7 @@
           }
 
           var height = byCodes('8302-2');
+          var weight = byCodes('29463-7'); //ryan
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
@@ -49,7 +50,7 @@
           p.fname = fname;
           p.lname = lname;
           p.height = getQuantityValueAndUnit(height[0]);
-          p.weight = weight; //ryan
+          p.weight = getQuantityValueAndUnit(weight[0]); //ryan
 
           if (typeof systolicbp != 'undefined')  {
             p.systolicbp = systolicbp;
@@ -123,9 +124,9 @@
     $('#fname').html(p.fname);
     $('#lname').html(p.lname);
     $('#gender').html(p.gender);
-    $('#weight').html(p.weight);
     $('#birthdate').html(p.birthdate);
     $('#height').html(p.height);
+    $('#weight').html(p.weight); //ryan
     $('#systolicbp').html(p.systolicbp);
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
